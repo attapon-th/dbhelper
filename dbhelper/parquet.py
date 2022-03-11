1cd2c5a3533b8bc30b1828e9bd2c39d72232285e
Attapon Thanawong <attapon_srem@hotmail.com>
2022-03-11 14:13:18 +0700

    Modified: README.MD, dbhelper/parquet.py, setup.py

--- dbhelper/parquet.py
+++ dbhelper/parquet.py
@@ -24,52 +24,58 @@ def create_parquet_file(engine, sql_query: str, file_name: os.PathLike, compress
     if callback_print is None:
         def callback_print(*v, **k):
             pass
-    dir_temp = "./temp"
-    temp_file = os.path.join(dir_temp, str(time.time()))
-    os.makedirs(dir_temp, exist_ok=True)
-    callback_print("Start Query SqlStantement")
-    iterator = pd.read_sql_query(
-        sql_query,
-        engine,
-        coerce_float=True,
-        chunksize=10000,
-        # dtype=pd.StringDtype(storage="pyarrow"),
-    )
-    callback_print("End Query SqlStantement")
+    try:
+        dir_temp = "./temp_" + str(time.time())
+        temp_file = os.path.join(dir_temp, str(time.time()))
+        os.makedirs(dir_temp, exist_ok=True)
+        callback_print("Start Query SqlStantement")
+        iterator = pd.read_sql_query(
+            sql_query,
+            engine,
+            coerce_float=True,
+            chunksize=10000,
+            # dtype=pd.StringDtype(storage="pyarrow"),
+        )
+        callback_print("End Query SqlStantement")
 
-    total_count = 0
-    for i, df in enumerate(iterator):
-        callback_print("Convert To Parquet chunk: ", (i+1))
-        if df is not None:
-            callback_print("Rows Count: ", len(df.index))
+        total_count = 0
+        for i, df in enumerate(iterator):
+            callback_print("Convert To Parquet chunk: ", (i+1))
+            if df is not None:
+                callback_print("Rows Count: ", len(df.index))
 
-        df = dfh.convert_dtypes(df)
+            df = dfh.convert_dtypes(df)
 
-        callback_print("Pandas to parquet file.")
-        table = pa.Table.from_pandas(df, preserve_index=False)
-        # print(table)
-        # for the first chunk of records
-        if i == 0:
-            # print(df.dtypes)
-            # create a parquet write object giving it an output file
-            pqwriter = pq.ParquetWriter(
-                temp_file, table.schema, compression=compression)
-            pqwriter.write_table(table)
-        # subsequent chunks can be written to the same file
-        else:
-            pqwriter.write_table(table)
-        total_count += len(df.index)
+            callback_print("Pandas to parquet file.")
+            table = pa.Table.from_pandas(df, preserve_index=False)
+            # print(table)
+            # for the first chunk of records
+            if i == 0:
+                # print(df.dtypes)
+                # create a parquet write object giving it an output file
+                pqwriter = pq.ParquetWriter(
+                    temp_file, table.schema, compression=compression)
+                pqwriter.write_table(table)
+            # subsequent chunks can be written to the same file
+            else:
+                pqwriter.write_table(table)
+            total_count += len(df.index)
 
-    # close the parquet writer
-    if pqwriter:
-        pqwriter.close()
+        # close the parquet writer
+        if pqwriter:
+            pqwriter.close()
 
-    engine.close()
+        engine.close()
 
-    callback_print("Move file.")
-    # ยายไฟล์จาก temfile
-    os.rename(temp_file, file_name)
-    callback_print("End.")
+        callback_print("Move file.")
+        # ยายไฟล์จาก temfile
+        os.rename(temp_file, file_name)
+        callback_print("End.")
+    except Exception as ex:
+        raise ex
+    finally:
+        if os.path.exists(dir_temp):
+            os.unlink(dir_temp)
 
 
 def read_parquet(filename: os.PathLike) -> pd.DataFrame:
