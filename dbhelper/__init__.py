1d2a47ddc2e1c6fe7dbf2d82172068b334ca04d6
Attapon Thanawong <attapon_srem@hotmail.com>
2022-05-25 10:34:20 +0700

    Modified: dbhelper/__init__.py

--- dbhelper/__init__.py
+++ dbhelper/__init__.py
@@ -3,8 +3,6 @@ from . import dataframe
 from . import parquet
 from . import vertica
 from . import csv
-from setup import version_info
 
-__version__ = '.'.join(map(str, version_info))
 
-__all__ = ['connection', 'dataframe', 'parquet', 'vertica', 'csv', 'version_info']
+__all__ = ['connection', 'dataframe', 'parquet', 'vertica', 'csv']
